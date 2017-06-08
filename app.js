c2330b9fb2c56809d2c218b4662f570eeb0475ea
Renan Mariano <renan.mariano@f2f-digital.com>
2017-06-08 12:54:04 -0300

    heroku

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ function receivedMessage(event)  {
         // If we receive a text message, check to see if it matches a keyword
         // and send back the example. Otherwise, just echo the text we received.
         switch (messageText) {
-            case 'generic':
+            case 'agenda':
             sendGenericMessage(senderID);
             break;
 
@@ -105,50 +105,42 @@ function receivedMessage(event)  {
 }
 
 function sendGenericMessage(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "generic",
+                    elements: [{
+                        title: "QUINTA-FEIRA 15/06",
+                        subtitle: "CAU E DEBORAH",
+                        item_url: "http://www.villacountry.com.br/programacao_interno_descricao.php?id=51&c_data=20170615#Agenda",
+                        image_url: "http://www.villacountry.com.br/upload/artista/51.jpg",
+                        buttons: [{
+                            type: "web_url",
+                            url: "http://www.villacountry.com.br/programacao_interno_descricao.php?id=51&c_data=20170615#Agenda",
+                            title: "Comprar ingresso"
+                        }],
+                    }, {
+                        title: "GIANCARLO e JULIANO",
+                        subtitle: "SEXTA-FEIRA 16/06",
+                        item_url: "http://www.villacountry.com.br/programacao_interno_descricao.php?id=21&c_data=20170616#Agenda",
+                        image_url: "http://www.villacountry.com.br/upload/artista/21.jpg",
+                        buttons: [{
+                            type: "web_url",
+                            url: "http://www.villacountry.com.br/programacao_interno_descricao.php?id=21&c_data=20170616#Agenda",
+                            title: "Comprar ingresso"
+                        }]
+                    }]
+                }
+            }
         }
-      }
-    }
-  };
+    };
 
-  callSendAPI(messageData);
+    callSendAPI(messageData);
 }
 
 function sendTextMessage(recipientId, messageText) {
@@ -187,18 +179,18 @@ function callSendAPI(messageData) {
 }
 
 function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
 
-  // The 'payload' param is a developer-defined field which is set in a postback
-  // button for Structured Messages.
-  var payload = event.postback.payload;
+    // The 'payload' param is a developer-defined field which is set in a postback
+    // button for Structured Messages.
+    var payload = event.postback.payload;
 
-  console.log("Received postback for user %d and page %d with payload '%s' " +
+    console.log("Received postback for user %d and page %d with payload '%s' " +
     "at %d", senderID, recipientID, payload, timeOfPostback);
 
-  // When a postback is called, we'll send a message back to the sender to
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+    // When a postback is called, we'll send a message back to the sender to
+    // let them know it was successful
+    sendTextMessage(senderID, "Postback called");
 }
